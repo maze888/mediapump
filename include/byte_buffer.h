a079cb63428343037d8cc2c45b055276e3362ba1
devcube <jejeje888@naver.com>
2026-04-01 14:14:55 +0900

    feat: add memory function and struct

--- include/byte_buffer.h
+++ include/byte_buffer.h
@@ -4,11 +4,16 @@
 
 struct byte_buffer {
     void *buf;
-    size_t len;
+    size_t cap;
+
+    unsigned char *wbuf;
+    size_t used;
 };
 
 struct byte_buffer * alloc_byte_buffer(size_t len);
-struct byte_buffer * realloc_byte_buffer(struct byte_buffer *bb, size_t len);
-void free_byte_buffer(struct byte_buffer *bb);
+int realloc_byte_buffer(struct byte_buffer *bb, size_t size);
+void free_byte_buffer(struct byte_buffer **bb);
+
+size_t write_byte_buffer(struct byte_buffer *bb, void *buf, size_t len);
 
 
--- src/byte_buffer.c
+++ src/byte_buffer.c
@@ -1,7 +1,9 @@
 #include "byte_buffer.h"
 #include "logging.h"
 
-struct byte_buffer * alloc_byte_buffer(size_t len)
+#include <string.h>
+
+struct byte_buffer * alloc_byte_buffer(size_t size)
 {
     struct byte_buffer *bb = malloc(sizeof(struct byte_buffer));
     if (!bb) {
@@ -9,38 +11,65 @@ struct byte_buffer * alloc_byte_buffer(size_t len)
         abort();
     }
 
-    bb->buf = malloc(len);
+    bb->buf = malloc(size);
     if (!bb->buf) {
-        LOG_CRIT("malloc(%zu) failed", len);
+        LOG_CRIT("malloc(%zu) failed", size);
         abort();
     }
-    bb->len = len;
+    bb->cap = size;
+
+    bb->wbuf = bb->buf;
+    bb->used = 0;
 
     return bb;
 }
 
-void realloc_byte_buffer(struct byte_buffer *bb, size_t len)
+int realloc_byte_buffer(struct byte_buffer *bb, size_t size)
 {
-    if (!bb || len == 0) {
-        LOG_ERROR("realloc_byte_buffer() failed: byte_buffer = %p, len = %zu", bb, len);
-        return;
+    if (!bb || size == 0) {
+        LOG_ERROR("realloc_byte_buffer() failed: byte_buffer = %p, size = %zu", bb, size);
+        return -1;
     }
 
-    void *tmp = realloc(bb->buf, len);
-
-    if (!tmp) {
-        LOG_CRIT("realloc(%zu) failed. Original ptr: %p", len, bb->buf);
+    void *p = realloc(bb->buf, size);
+    if (!p) {
+        LOG_CRIT("realloc(%zu) failed. Original ptr: %p", size, bb->buf);
         abort(); 
     }
 
-    bb->buf = tmp;
-    bb->len = len;
+    bb->buf = p;
+    bb->cap = size;
+
+    bb->wbuf = bb->buf + bb->used;
+
+    return 0;
 }
 
-void free_byte_buffer(struct byte_buffer *bb)
+void free_byte_buffer(struct byte_buffer **bb)
 {
-    if (bb) {
-        safe_free(bb->buf);
-        safe_free(bb);
+    if (bb && *bb) {
+        safe_free((*bb)->buf);
+        safe_free(*bb);
     }
+}
+
+size_t write_byte_buffer(struct byte_buffer *bb, void *src, size_t src_size)
+{
+    if (!bb || !src || src_size == 0) {
+        LOG_ERROR("write_byte_buffer() failed: invalid argument: byte_buffer = %p, src = %p, src_size = %zu", bb, src, src_size);
+        return 0;
+    }
+
+    if ((bb->cap - bb->used) < src_size) {
+        if (realloc_byte_buffer(bb, bb->cap * 2) < 0) {
+            LOG_ERROR("write_byte_buffer() failed: alloc error: byte_buffer = %p, src = %p, src_size = %zu", bb, src, src_size);
+            return 0;
+        }
+    }
+
+    memcpy(bb->wbuf, src, src_size);
+    bb->wbuf += src_size;
+    bb->used += src_size;
+
+    return src_size;
 }
