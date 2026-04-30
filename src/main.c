1c9ec6c590952ec1a62383cdbeff00ebd37c28cd
devcube <jejeje888@naver.com>
2026-04-30 15:03:18 +0000

    build(dev): configure LSP and memory sanitizers for development

--- src/main.c
+++ src/main.c
@@ -1,13 +1,17 @@
 #include "logging.h"
+#include "byte_buffer.h"
 
 int main(int argc, char **argv) {
     setvbuf(stdout, NULL, _IOLBF, 0);
 
     LOG_CRIT("good....");
     LOG_CRIT("hi there %d", 13);
+    
     LOG_ERROR("hi there %s", "good man");
     LOG_INFO("hi there %s %d", "good man", 256);
     LOG_DEBUG("hi there %s %d %d %s", "good man", 256, 512, "world");
+
+    struct byte_buffer *bb = alloc_byte_buffer(1024);
 
     return 0;
 }
